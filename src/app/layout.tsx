e0b1ad41188694605b4f3ca640f89f35869bad0b
Elvis-Houssou <houssouelvis@gmail.com>
2026-05-17 21:37:30 +0000

    fix rul

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
   variable: '--font-inter'
 });
 
-const BASE_URL = "https://portfolio-jpwl.vercel.app/"
+const BASE_URL = "https://elvis-houssou.vercel.app/"
 // const BASE_URL = "https://elvishoussou.dev"
 
 export const metadata: Metadata = {
